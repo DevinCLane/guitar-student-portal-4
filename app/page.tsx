1d50457bb2966c0cbe6eef89d1c6a5e1222e8ef4
Devin Lane <devin.c.lane@gmail.com>
2024-10-28 14:44:51 -0700

    sign in with google

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import SignIn from "@/components/sign-in";
+
 export default function Home() {
     return (
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            hello
+            <SignIn />
         </main>
     );
 }
